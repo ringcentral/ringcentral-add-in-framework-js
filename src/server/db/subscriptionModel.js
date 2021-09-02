6ede3ab05582e98586ef5b1b40a4e0caa8b20860
da kong <kongda9999@gmail.com>
2021-09-02 09:01:41 +0800

    fix typo

--- src/server/db/subscriptionModel.js
+++ src/server/db/subscriptionModel.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const { sequelize } = require('./sequelize');
 
 // Model for Subscription data
-exports.Subscription = sequelize.define('subcriptions', {
+exports.Subscription = sequelize.define('subscriptions', {
   id: {
     type: Sequelize.STRING,
     primaryKey: true,
